fb859c668dd66b72790125918add505fa39a8b42
Andy O'Donald <andyo@Andys-MacBook-Pro.local>
2016-01-13 08:25:57 -0800

    Average updates

--- average.cpp
+++ average.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-	double num1 = 0;
-	double num2 = 0;
-	double num3 = 0;
-	double num4 = 0;
-	double num5 = 0;
-	double sum = 0;
+	double num1;
+	double num2;
+	double num3;
+	double num4;
+	double num5;
+	double sum;
 	int denominator = 5;
-	double average = 0;
+	double average;
 	
 	cout << "Please enter five numbers." << endl; 
 	cin >> num1;
@@ -19,7 +19,7 @@ int main()
 	cin >> num4;
 	cin >> num5;
 	
-	sum = num1 + num2 +num3 + num4 + num5;
+	sum = num1 + num2 + num3 + num4 + num5;
 	average = sum / denominator;
 	
 	cout << "The average of those numbers is: \n" << average << endl;
