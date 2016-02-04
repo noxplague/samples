c2343d924f45f8b63b89668e6590c2aebec5ab2e
U-REDMOND\aodona <aodona@ANDYOT430S.redmond.corp.microsoft.com>
2016-02-03 18:11:26 -0800

    Near final

--- bankMain.cpp
+++ bankMain.cpp
@@ -9,37 +9,43 @@ using std::string;
 
 int main() {
 	
-	double userAmount;
+	double userAmount, userAmount2;
 	
 	BankAccount account1;
 	
-	cout << "Customer name:" << account1.getCustomerName << endl;
-	cout << "Customer ID:" << account1.getCustomerID << endl;
-	cout << "Customer balance:" << account1.getCustomerBalance << endl;
+	cout << "Customer name:" << account1.getCustomerName() << endl;
+	cout << "Customer ID:" << account1.getCustomerID() << endl;
+	cout << "Customer balance:" << account1.getCustomerBalance() << endl;
 	
 	cout << "How much would you like to deposit: " << endl;
 	cin >> userAmount;
-	BankAccount::deposit(userAmount);
-	cout << "Customer balance:" << account1.getCustomerBalance << endl;
+	account1.deposit(userAmount);
+	cout << "Customer balance:" << account1.getCustomerBalance() << endl;
 	
 	
 	string userName, userID;
 	double userBalance;
 	
-	cout << "Create a new account, please enter a Name, ID, and" ;
+	cout << "Create a new account, please enter a Name, ID, and " ;
 	cout << "starting account balance: " << endl;
 	cin >> userName >> userID >> userBalance;
 	BankAccount account2(userName, userID, userBalance);
 	
-	cout << "Customer name:" << account2.getCustomerName << endl;
-	cout << "Customer ID:" << account2.getCustomerID << endl;
-	cout << "Customer balance:" << account2.getCustomerBalance << endl;
+	cout << "Customer name: " << account2.getCustomerName() << endl;
+	cout << "Customer ID: " << account2.getCustomerID() << endl;
+	cout << "Customer balance: " << account2.getCustomerBalance() << endl;
 	
+	
+	
+	cout << "How much would you like to deposit: " << endl;
+	cin >> userAmount;
+	account2.deposit(userAmount);
+	cout << "Customer balance:" << account2.getCustomerBalance() << endl;
 	
 	cout << "How much would you like to withdraw: " << endl;
-	cin >> userAmount;
-	BankAccount::withdraw(userAmount);
-	cout << "Customer balance:" << account1.getCustomerBalance << endl;
+	cin >> userAmount2;
+	account2.withdraw(userAmount2);
+	cout << "Customer balance: " << account2.getCustomerBalance() << endl;
 	
 	return 0;
 	
